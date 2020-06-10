1bd1bb7f9d338fde2ac961621b61ff2634a5e173
Jan Bartsch <jan.bartsch@mathematik.uni-wuerzburg.de>
2020-06-10 18:01:11 +0200

    Tried to run shifting negative on CSSC - not working

--- src/controller/desired_trajectory_controller.cpp
+++ src/controller/desired_trajectory_controller.cpp
@@ -61,9 +61,9 @@ std::vector<double> desired_trajectory_controller::trajectory_desired_shifting_h
                 barycenter[1]*barycenter[1]+
                 barycenter[2]*barycenter[2];
     }
-    p_d[1] = 200.0;
-    p_d[2] = 200.0;
-    p_d[3] = 200.0;
+    p_d[1] = -100.0;
+    p_d[2] = 100.0;
+    p_d[3] = 100.0;
 
     return p_d;
 
