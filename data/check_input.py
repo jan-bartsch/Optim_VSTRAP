5304f2bd91198b9456a4688d2a5c64fbc5497257
jab56qk <jan.bartsch@uni-wuerzburg.de>
2020-06-22 15:52:01 +0200

    added input check, changed dir for running run_tests

---
+++ data/check_input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import argparse
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
+
+
+parser = argparse.ArgumentParser(prog="Check if xml-arguments fit together", description='Needs data directory (with input files)')
+parser.add_argument('inputDir', type=str, help='path to input files')
+args = parser.parse_args()
+
+OptimIn = minidom.parse(args.inputDir + "Optim_input.xml");
+parameters = OptimIn.getElementsByTagName('globalParameters');
+
+print(parameters)
