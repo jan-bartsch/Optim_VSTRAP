e64330df9179a34ad70560e275c59a2910304550
jab56qk <jan.bartsch@uni-wuerzburg.de>
2020-06-22 16:49:17 +0200

    changed dir for run test

--- data/check_input.py
+++ data/check_input.py
@@ -14,12 +14,22 @@ parameters = OptimIn.getElementsByTagName('globalParameters')[0];
 paths = OptimIn.getElementsByTagName('paths')[0];
 subroutines = OptimIn.getElementsByTagName('subroutines')[0];
 
+params = [];
+i = 0;
+
+for p in parameters.getElementsByTagName('parameter'):
+	p_data = p.getAttribute("name")
+	print(i)
+	params.append([p.getAttribute("name"),p.getAttribute("value")])
+	i = i+1
+
 forwardIn = minidom.parse(args.inputDir + "input_forward.xml");
 backwardIn = minidom.parse(args.inputDir + "input_backward.xml");
 
 forward_timeStep = forwardIn.getElementsByTagName('global_values')[0].getElementsByTagName('time_step')[0].getAttribute("value")
 backward_timeStep = backwardIn.getElementsByTagName('global_values')[0].getElementsByTagName('time_step')[0].getAttribute("value")
-Optim_timestep = parameters.getAttributeNode("dt_gp")
+
+print(params)
 
 print(Optim_timestep)
 
