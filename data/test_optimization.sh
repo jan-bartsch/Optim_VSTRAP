38fc7deb61b60bb8205d6ff416324e26a371857b
jab56qk <jan.bartsch@uni-wuerzburg.de>
2020-07-01 23:08:13 +0200

    Prepared for testcase A3xx

--- data/test_optimization.sh
+++ data/test_optimization.sh
@@ -1,27 +1,27 @@
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A201.xml
-echo "Copying results A201"
-rm -r results_A201 && mkdir results_A201
-cp -r ./results/ ./results_A201
-cp ../../Optim_VSTRAP/data/box_center_CSSC/interpolated_control_field.xml ./results_A201
-cp ../../Optim_VSTRAP/data/box_center_CSSC/control_field_cells.xml ./results_A201
-cp ../../Optim_VSTRAP/data/box_center_CSSC/creation_adjoint_particles.xml ./results_A201
-echo "Finished A201"
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A301.xml
+echo "Copying results A301"
+rm -r results_A301 && mkdir results_A301
+cp -r ./results/ ./results_A301
+cp ../../Optim_VSTRAP/data/box_center_CSSC/interpolated_control_field.xml ./results_A301
+cp ../../Optim_VSTRAP/data/box_center_CSSC/control_field_cells.xml ./results_A301
+cp ../../Optim_VSTRAP/data/box_center_CSSC/creation_adjoint_particles.xml ./results_A301
+echo "Finished A301"
 
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A202.xml
-echo "Copying results A202"
-rm -r results_A202 && mkdir results_A202
-cp -r ./results/ ./results_A202
-cp ../../Optim_VSTRAP/data/box_center_CSSC/interpolated_control_field.xml ./results_A202
-cp ../../Optim_VSTRAP/data/box_center_CSSC/control_field_cells.xml ./results_A202
-cp ../../Optim_VSTRAP/data/box_center_CSSC/creation_adjoint_particles.xml ./results_A202
-echo "Finished A202"
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A302.xml
+echo "Copying results A302"
+rm -r results_A302 && mkdir results_A302
+cp -r ./results/ ./results_A302
+cp ../../Optim_VSTRAP/data/box_center_CSSC/interpolated_control_field.xml ./results_A302
+cp ../../Optim_VSTRAP/data/box_center_CSSC/control_field_cells.xml ./results_A302
+cp ../../Optim_VSTRAP/data/box_center_CSSC/creation_adjoint_particles.xml ./results_A302
+echo "Finished A302"
 
 #screen ./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A3.xml
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A203.xml
-echo "Copying results A203"
-rm -r results_A203 && mkdir results_A203
-cp -r ./results/ ./results_A203
-cp ../../Optim_VSTRAP/data/box_center_CSSC/interpolated_control_field.xml ./results_A203
-cp ../../Optim_VSTRAP/data/box_center_CSSC/control_field_cells.xml ./results_A203
-cp ../../Optim_VSTRAP/data/box_center_CSSC/creation_adjoint_particles.xml ./results_A203
-echo "Finished A203"
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A303.xml
+echo "Copying results A303"
+rm -r results_A303 && mkdir results_A303
+cp -r ./results/ ./results_A303
+cp ../../Optim_VSTRAP/data/box_center_CSSC/interpolated_control_field.xml ./results_A303
+cp ../../Optim_VSTRAP/data/box_center_CSSC/control_field_cells.xml ./results_A303
+cp ../../Optim_VSTRAP/data/box_center_CSSC/creation_adjoint_particles.xml ./results_A303
+echo "Finished A303"
