38f108de1038d835b6467c999892498b10b04874
jab56qk <jan.bartsch@uni-wuerzburg.de>
2020-08-11 20:54:13 +0200

    Prepared for run on local data

--- data/test_optimization.sh
+++ data/test_optimization.sh
@@ -1,27 +1,27 @@
 echo -e "\e[32mStarting A10xx\e[39m"
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A1001.xml
-echo -e "\e[32mCopying results A1001\e[39m"
-rm -r results_A1001 && mkdir results_A1001
-cp -r ./results/ ./results_A1001
-cp ../../Optim_VSTRAP/data/box_center_CSSC/interpolated_control_field.xml ./results_A1001
-cp ../../Optim_VSTRAP/data/box_center_CSSC/control_field_cells.xml ./results_A1001
-cp ../../Optim_VSTRAP/data/box_center_CSSC/creation_adjoint_particles.xml ./results_A1001
-echo -e "\e[32mFinishedA1001\e[39m"
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_CSSC/Optim_input_B1001.xml
+echo -e "\e[32mCopying results B1001\e[39m"
+rm -r results_B1001 && mkdir results_B1001
+cp -r ./results/ ./results_B1001
+cp ../../Optim_VSTRAP/data/box_shifting_CSSC/interpolated_control_field.xml ./results_B1001
+cp ../../Optim_VSTRAP/data/box_shifting_CSSC/control_field_cells.xml ./results_B1001
+cp ../../Optim_VSTRAP/data/box_shifting_CSSC/creation_adjoint_particles.xml ./results_B1001
+echo -e "\e[32mFinishedB1001\e[39m"
 
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A1002.xml
-echo -e "\e[32mCopying results A1002\e[39m"
-rm -r results_A1002 && mkdir results_A1002
-cp -r ./results/ ./results_A1002
-cp ../../Optim_VSTRAP/data/box_center_CSSC/interpolated_control_field.xml ./results_A1002
-cp ../../Optim_VSTRAP/data/box_center_CSSC/control_field_cells.xml ./results_A1002
-cp ../../Optim_VSTRAP/data/box_center_CSSC/creation_adjoint_particles.xml ./results_A1002
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_CSSC/Optim_input_B1002.xml
+echo -e "\e[32mCopying results B1002\e[39m"
+rm -r results_B1002 && mkdir results_B1002
+cp -r ./results/ ./results_B1002
+cp ../../Optim_VSTRAP/data/box_shifting_CSSC/interpolated_control_field.xml ./results_B1002
+cp ../../Optim_VSTRAP/data/box_shifting_CSSC/control_field_cells.xml ./results_B1002
+cp ../../Optim_VSTRAP/data/box_shifting_CSSC/creation_adjoint_particles.xml ./results_B1002
 echo -e "\e[32mFinished A1002\e[39m"
 
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_center_CSSC/Optim_input_A1003.xml
-echo -e "\e[32mCopying results A1003\e[39m"
-rm -r results_A1003 && mkdir results_A1003
-cp -r ./results/ ./results_A1003
-cp ../../Optim_VSTRAP/data/box_center_CSSC/interpolated_control_field.xml ./results_A1003
-cp ../../Optim_VSTRAP/data/box_center_CSSC/control_field_cells.xml ./results_A1003
-cp ../../Optim_VSTRAP/data/box_center_CSSC/creation_adjoint_particles.xml ./results_A1003
-echo -e "\e[32mFinished A1003\e[39m"
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_CSSC/Optim_input_B1003.xml
+echo -e "\e[32mCopying results B1003\e[39m"
+rm -r results_A1003 && mkdir results_B1003
+cp -r ./results/ ./results_B1003
+cp ../../Optim_VSTRAP/data/box_shifting_CSSC/interpolated_control_field.xml ./results_B1003
+cp ../../Optim_VSTRAP/data/box_shifting_CSSC/control_field_cells.xml ./results_B1003
+cp ../../Optim_VSTRAP/data/box_shifting_CSSC/creation_adjoint_particles.xml ./results_B1003
+echo -e "\e[32mFinished B1003\e[39m"
