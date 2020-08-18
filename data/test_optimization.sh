6fbc8dc662418783127e9aaef875ed6bfdb63023
jab56qk <jan.bartsch@uni-wuerzburg.de>
2020-08-18 12:16:50 +0200

    Changed test sh

--- data/test_optimization.sh
+++ data/test_optimization.sh
@@ -1,13 +1,13 @@
-echo -e "\e[32mStarting A10xx\e[39m"
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_CSSC/Optim_input_B1001.xml
-echo -e "\e[32mCopying results B1001\e[39m"
-rm -r results_B1001 
-mkdir results_B1001
-cp -r ./results/ ./results_B1001
-cp ../../Optim_VSTRAP/data/box_shifting_CSSC/interpolated_control_field.xml ./results_B1001
-cp ../../Optim_VSTRAP/data/box_shifting_CSSC/control_field_cells.xml ./results_B1001
-cp ../../Optim_VSTRAP/data/box_shifting_CSSC/creation_adjoint_particles.xml ./results_B1001
-echo -e "\e[32mFinishedB1001\e[39m"
+#echo -e "\e[32mStarting A10xx\e[39m"
+#./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_CSSC/Optim_input_B1001.xml
+#echo -e "\e[32mCopying results B1001\e[39m"
+#rm -r results_B1001 
+#mkdir results_B1001
+#cp -r ./results/ ./results_B1001
+#cp ../../Optim_VSTRAP/data/box_shifting_CSSC/interpolated_control_field.xml ./results_B1001
+#cp ../../Optim_VSTRAP/data/box_shifting_CSSC/control_field_cells.xml ./results_B1001
+#cp ../../Optim_VSTRAP/data/box_shifting_CSSC/creation_adjoint_particles.xml ./results_B1001
+#echo -e "\e[32mFinishedB1001\e[39m"
 
 ./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_CSSC/Optim_input_B1002.xml
 echo -e "\e[32mCopying results B1002\e[39m"
