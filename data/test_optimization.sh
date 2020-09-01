e0bf5b5b3022dc1770944e07b2b2ea949a9a7fff
Jan <jan.bartsch@mathematik.uni-wuerzburg.de>
2020-09-01 10:54:15 +0200

    Prepared B3xx

--- data/test_optimization.sh
+++ data/test_optimization.sh
@@ -1,30 +1,30 @@
 echo -e "\e[32mStarting B2xx\e[39m"
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_temp/Optim_input_B2001.xml
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_temp/Optim_input_B3001.xml
 echo -e "\e[32mCopying results\e[39m"
-rm -r results_B2001 
-mkdir results_B2001
-cp -r ./results/ ./results_B2001
-cp ../../Optim_VSTRAP/data/box_shifting_temp/interpolated_control_field.xml ./results_B2001
-cp ../../Optim_VSTRAP/data/box_shifting_temp/control_field_cells.xml ./results_B2001
-cp ../../Optim_VSTRAP/data/box_shifting_temp/creation_adjoint_particles.xml ./results_B2001
+rm -r results_B3001 
+mkdir results_B3001
+cp -r ./results/ ./results_B3001
+cp ../../Optim_VSTRAP/data/box_shifting_temp/interpolated_control_field.xml ./results_B3001
+cp ../../Optim_VSTRAP/data/box_shifting_temp/control_field_cells.xml ./results_B3001
+cp ../../Optim_VSTRAP/data/box_shifting_temp/creation_adjoint_particles.xml ./results_B3001
 echo -e "\e[32mFinishedB1001\e[39m"
 
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_temp/Optim_input_B2002.xml
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_temp/Optim_input_B3002.xml
 echo -e "\e[32mCopying results \e[39m"
-rm -r results_B2002 
-mkdir results_B2002
-cp -r ./results/ ./results_B2002
-cp ../../Optim_VSTRAP/data/box_shifting_temp/interpolated_control_field.xml ./results_B2002
-cp ../../Optim_VSTRAP/data/box_shifting_temp/control_field_cells.xml ./results_B2002
-cp ../../Optim_VSTRAP/data/box_shifting_temp/creation_adjoint_particles.xml ./results_B2002
+rm -r results_B3002 
+mkdir results_B3002
+cp -r ./results/ ./results_B3002
+cp ../../Optim_VSTRAP/data/box_shifting_temp/interpolated_control_field.xml ./results_B3002
+cp ../../Optim_VSTRAP/data/box_shifting_temp/control_field_cells.xml ./results_B3002
+cp ../../Optim_VSTRAP/data/box_shifting_temp/creation_adjoint_particles.xml ./results_B3002
 echo -e "\e[32mFinished A1002\e[39m"
 
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_temp/Optim_input_B2003.xml
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_temp/Optim_input_B3003.xml
 echo -e "\e[32mCopying results \e[39m"
-rm -r results_B2003 
-mkdir results_B2003
-cp -r ./results/ ./results_B2003
-cp ../../Optim_VSTRAP/data/box_shifting_temp/interpolated_control_field.xml ./results_B2003
-cp ../../Optim_VSTRAP/data/box_shifting_temp/control_field_cells.xml ./results_B2003
-cp ../../Optim_VSTRAP/data/box_shifting_temp/creation_adjoint_particles.xml ./results_B2003
+rm -r results_B3003 
+mkdir results_B3003
+cp -r ./results/ ./results_B3003
+cp ../../Optim_VSTRAP/data/box_shifting_temp/interpolated_control_field.xml ./results_B3003
+cp ../../Optim_VSTRAP/data/box_shifting_temp/control_field_cells.xml ./results_B3003
+cp ../../Optim_VSTRAP/data/box_shifting_temp/creation_adjoint_particles.xml ./results_B3003
 echo -e "\e[32mFinished \e[39m"
