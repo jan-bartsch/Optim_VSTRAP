40cf3c327e7b867b592828e815df05f1455aed9c
Jan Bartsch <jan.bartsch@mathematik.uni-wuerzburg.de>
2020-09-24 16:56:32 +0200

    Added 216_weight test

---
+++ data/box_shifting_weight/test_weight_216.sh
@@ -0,0 +1,12 @@
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_weight/Optim_input_CSMC_216_50.xml
+cd ../../build-Optim-CSMC2/src
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_weight/Optim_input_CSMC_216_150.xml
+cd ../../build-Optim-CSMC3/src
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_weight/Optim_input_CSMC_216_250.xml
+cd ../../build-Optim-CSMC4/src
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_weight/Optim_input_CSMC_216_350.xml
+cd ../../build-Optim-CSMC5/src
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_weight/Optim_input_CSMC_216_450.xml
+cd ../../build-Optim-CSMC6/src
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/box_shifting_weight/Optim_input_CSMC_216_550.xml
+
