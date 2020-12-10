6d73e70340930f262603780b443e5803282234cc
Jan Bartsch <jan.bartsch@mathematik.uni-wuerzburg.de>
2020-12-10 09:13:12 +0100

    generation adjoint

--- optim-vstrap-toolset/toolset/generate_adjoint_particle_creation.py
+++ optim-vstrap-toolset/toolset/generate_adjoint_particle_creation.py
@@ -55,15 +55,15 @@ v_s_z = sigma_v;
 
 file.write("<parameraters>\n")
 
-#for timestep in range(0,ntimesteps):
-#	file.write("\t<set iteration=\"" + str(timestep) + "\">\n")
-#	file.write("\t\t<particle_values number_density=\""+str(params["adjoint_number_density"])+" \" weight=\""+str(params["adjoint_weight"])+"\" charge_number=\""+str(params["adjoint_charge_number"])+"\" mass=\""+str(params["adjoint_mass"])+"\" species=\""+str(params["adjoint_species"])+"\"/>\n")
-#	file.write("\t\t<position>\n \t\t\t<mu x_val = \"" + str(mu_x) + "\" y_val = \"" + str(mu_y) + "\" z_val = \"" + str(mu_z) + "\" />\n")
-#	file.write("\t\t\t<sigma x_val = \"" + str(s_x) +"\" y_val = \"" + str(s_y)+ "\" z_val = \"" + str(s_z) +"\"/> \n \t\t</position>\n")
-#	file.write("\t\t<velocity> \n \t\t\t<mu x_val = \"" + str(v_x) + " \" y_val = \"" + str(v_y) + "\" z_val = \"" + str(v_z) + "\" />\n")
-#	file.write("\t\t\t<sigma x_val = \"" + str(v_s_x) +"\" y_val = \"" + str(v_s_y)+ "\" z_val = \"" + str(v_s_z) +"\"/> \n \t\t</velocity>\n")
-#	file.write("\t</set>\n")
-#file.write("</parameraters>")
+for timestep in range(0,ntimesteps):
+	file.write("\t<set iteration=\"" + str(timestep) + "\">\n")
+	file.write("\t\t<particle_values number_density=\""+str(params["adjoint_number_density"])+" \" weight=\""+str(params["adjoint_weight"])+"\" charge_number=\""+str(params["adjoint_charge_number"])+"\" mass=\""+str(params["adjoint_mass"])+"\" species=\""+str(params["adjoint_species"])+"\"/>\n")
+	file.write("\t\t<position>\n \t\t\t<mu x_val = \"" + str(mu_x) + "\" y_val = \"" + str(mu_y) + "\" z_val = \"" + str(mu_z) + "\" />\n")
+	file.write("\t\t\t<sigma x_val = \"" + str(s_x) +"\" y_val = \"" + str(s_y)+ "\" z_val = \"" + str(s_z) +"\"/> \n \t\t</position>\n")
+	file.write("\t\t<velocity> \n \t\t\t<mu x_val = \"" + str(v_x) + " \" y_val = \"" + str(v_y) + "\" z_val = \"" + str(v_z) + "\" />\n")
+	file.write("\t\t\t<sigma x_val = \"" + str(v_s_x) +"\" y_val = \"" + str(v_s_y)+ "\" z_val = \"" + str(v_s_z) +"\"/> \n \t\t</velocity>\n")
+	file.write("\t</set>\n")
+file.write("</parameraters>")
 
 v_x = 1000; #2.0*float(params['pmax_gp'])/(float(params['ntimesteps_gp'])*float(params['dt_gp']))
 v_y = 0.0;
@@ -75,6 +75,7 @@ mu_z = 0.0;
 
 dt_gp = float(params['dt_gp']);
 
+"""
 for timestep in range(0,ntimesteps):
 	s_x = 0.01
 	s_y = 2*0.00015/(-0.05+0.1/(float(params['ntimesteps_gp']))*timestep+0.06)
@@ -92,3 +93,4 @@ for timestep in range(0,ntimesteps):
 file.write("</parameraters>")
 brockett_file.close()
 file.close()
+"""
