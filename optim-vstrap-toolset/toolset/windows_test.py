761734ad322e96ad7b455b3d6371e06b355ddf07
Jan Bartsch <jan.bartsch@mathematik.uni-wuerzburg.de>
2021-03-10 18:55:27 +0100

    on windows

--- optim-vstrap-toolset/toolset/windows_test.py
+++ optim-vstrap-toolset/toolset/windows_test.py
@@ -7,3 +7,4 @@ print("Hello World")
 #.\python.exe D:\SPARC\Optim_VSTRAP\optim-vstrap-toolset\toolset\visualize_control.py 
 #D:\SPARC\CSMC3\interpolated_control_field_216.xml D:\SPARC\Optim_VSTRAP\data\global\box_vol_regular_216.xml 0.01
 
+.\python.exe D:\SPARC\Optim_VSTRAP\optim-vstrap-toolset\toolset\visualize_control.py  D:\SPARC\magnetic_numeric\results\interpolated_control_field.xml D:\SPARC\Optim_VSTRAP\data\global\box_vol_regular_small_216.xml 0.01 D:\SPARC\magnetic_numeric\results\ 0.05
