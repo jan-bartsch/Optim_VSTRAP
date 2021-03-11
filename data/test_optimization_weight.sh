b9b051ba17822e11fc5966e5121d30190ccf0c67
Jan Bartsch <jan.bartsch@mathematik.uni-wuerzburg.de>
2021-03-11 09:04:06 +0100

    ready for position test

---
+++ data/test_optimization_weight.sh
@@ -0,0 +1,11 @@
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_73_1.xml
+rm -r -f results_73_1
+mkdir results_73_1
+cp -r ./results/ ./results_73_1
+
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_73_3.xml
+rm -r -f results_73_3
+mkdir results_73_3
+cp -r ./results/ ./results_73_3
+
+
