900adcf19e2867b4d9f33eb4f82d44db5b1a03ed
Jan Bartsch <jan.bartsch@mathematik.uni-wuerzburg.de>
2021-03-11 09:15:17 +0100

    correct naming

--- data/test_optimization_position.sh
+++ data/test_optimization_position.sh
@@ -1,19 +1,11 @@
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_75.xml
-rm -r -f results_75
-mkdir results_75
-cp -r ./results/ ./results_75
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_73_1.xml
+rm -r -f results_73_1
+mkdir results_73_1
+cp -r ./results/ ./results_73_1
 
-./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_76.xml
-rm -r -f results_76
-mkdir results_76
-cp -r ./results/ ./results_76
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_73_3.xml
+rm -r -f results_73_3
+mkdir results_73_3
+cp -r ./results/ ./results_73_3
 
-#./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_73.xml
-#rm -r -f results_73
-#mkdir results_73
-#cp -r ./results/ ./results_73
 
-#./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_74.xml
-#rm -r -f results_74
-#mkdir results_74
-#cp -r ./results/ ./results_74
