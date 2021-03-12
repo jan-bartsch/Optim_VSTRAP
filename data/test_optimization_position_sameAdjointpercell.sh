d4652de6f3f6e277fa97bd0e9f1e382228acb631
Jan Bartsch <jan.bartsch@mathematik.uni-wuerzburg.de>
2021-03-12 09:36:42 +0100

    Same adjoints per cell

---
+++ data/test_optimization_position_sameAdjointpercell.sh
@@ -0,0 +1,11 @@
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_73_1_1.xml
+rm -r -f results_73_1_1
+mkdir results_73_1_1
+cp -r ./results/ ./results_73_1_1
+
+./Optim_VSTRAP_CMAKE ../../Optim_VSTRAP/data/CS_test/Optim_input_73_3_1.xml
+rm -r -f results_73_3_1
+mkdir results_73_3_1
+cp -r ./results/ ./results_73_3_1
+
+
