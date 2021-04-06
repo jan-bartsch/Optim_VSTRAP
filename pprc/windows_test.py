d640e6ad85ae4e9ac138e986fc9baf2c17a22b3f
Jan Bartsch <jan.bartsch@mathematik.uni-wuerzburg.de>
2021-04-06 08:54:30 +0200

    Windows

---
+++ pprc/windows_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+print("Hello World")
+
+# C:\Users\Jan\AppData\Local\Programs\Python\Python38> 
+#.\python.exe D:\SPARC\Optim_VSTRAP\optim-vstrap-toolset\toolset\post_processing_convergence.py D:\SPARC\CSMC3\
+
+#.\python.exe D:\SPARC\Optim_VSTRAP\optim-vstrap-toolset\toolset\visualize_control.py 
+#D:\SPARC\CSMC3\interpolated_control_field_216.xml D:\SPARC\Optim_VSTRAP\data\global\box_vol_regular_216.xml 0.01
+
+.\python.exe D:\SPARC\Optim_VSTRAP\optim-vstrap-toolset\toolset\visualize_control.py  D:\SPARC\magnetic_numeric\results\interpolated_control_field.xml D:\SPARC\Optim_VSTRAP\data\global\box_vol_regular_small_216.xml 0.01 D:\SPARC\magnetic_numeric\results\ 0.05
